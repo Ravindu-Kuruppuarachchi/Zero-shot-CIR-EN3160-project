1b27fcc87297de63919e8eb23bc1674d1569ab7c
Ravindu Kuruppuarachchi <ravindukrashmika@gmail.com>
2025-10-21 08:59:29 +0530

    evaluating laion_cir_combined using fashionIq

--- config.py
+++ config.py
@@ -4,29 +4,36 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     dropout: float = 0.5 
-    num_layers: int = 2 
-    model_name: str = "blip" # [blip, clip-Vit-B/32, clip-Vit-L/14]
+    num_layers: int = 2
+    # In config.py
+    model_name: str = "clip-Vit-B/32" # Changed from "blip" 
+    #model_name: str = "blip" # [blip, clip-Vit-B/32, clip-Vit-L/14]
     device: torch.device = torch.device('cuda')
-    batch_size: int = 64  # you can adjust it according to your GPU memory
+    batch_size: int = 8  # you can adjust it according to your GPU memory
     encoder: str = 'text' # ['neither', 'text', 'both']
-    laion_type: str = 'laion_template' # ['laion_combined', 'laion_template', 'laion_llm'] choose different dataset
+    laion_type: str = 'laion_combined' # ['laion_combined', 'laion_template', 'laion_llm'] choose different dataset
     transform: str = 'targetpad'
     target_ratio: float = 1.25
     learning_rate: float = 1e-4
     weight_decay: float = 0.05
     adam_epsilon: float = 1e-8
-    num_epochs: int = 100
+    num_epochs: int = 20
     save_best: bool = True 
     use_amp: bool = True 
     validation_frequency: int = 1
-    comment: str = "fiq_test_template"
+    comment: str = "fiq_test_combined"
     dataset: str='fiq' # ['fiq', 'cirr']
-    save_path_prefix = ""
+    save_path_prefix = "/wandb"
     # eval related
-    eval_load_path: str="xxx"
-    #submission_name: str='fiq_test_finetune_blip_text_combined'
-    
+    eval_load_path: str= "D:/Documents 2.0/5th semester/computer vision/Vision Project/epoch_10_laion_combined.pth" 
+    submission_name: str='fiq_test_combined'
+
     # Set this to the folder where you want checkpoints to be saved
-    save_path_prefix: str = "D:/Documents 2.0/5th semester/computer vision/Vision Project/ZS-CIR/runs"
+    #save_path_prefix: str = "D:/Documents 2.0/5th semester/computer vision/Vision Project/ZS-CIR/runs"
     # A descriptive name for this training run
-    comment: str = "train_blip_text_fashion_subset_2k"
+    #comment: str = "train_blip_text_fashion_subset_2k"
+
+    # --- Paths (Not strictly needed for this eval method, but good practice) ---
+    #save_path_prefix = "D:/Documents 2.0/5th semester/computer vision/Vision Project/ZS-CIR/runs" # Or any valid path
+    #comment: str = "fiq_evaluation_run"
+
